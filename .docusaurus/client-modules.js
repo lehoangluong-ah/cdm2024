9456252a29a76a7eb1d40a89a47813f0a40a2188
Leo <luonglh@investidea.tech>
2024-05-23 15:03:27 +0700

    feat: deploy

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Volumes/BLACK/cdm2023/modules/AHSDK/documents/ah-sdk-doc/node_modules/infima/dist/css/default/default.css"),
+  require("/Volumes/BLACK/cdm2023/modules/AHSDK/documents/ah-sdk-doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Volumes/BLACK/cdm2023/modules/AHSDK/documents/ah-sdk-doc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Volumes/BLACK/cdm2023/modules/AHSDK/documents/ah-sdk-doc/src/css/custom.css"),
+];
