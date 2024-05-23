787bcf1499e59fac7b5c1fd1fb65631281b1d28d
ngocNC <ngoc-nc.nguyen@amplifyhealth.com>
2024-05-23 16:27:01 +0700

    change url link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://lehoangluong-ah.github.io/cdm2024/',
+  url: 'https://lehoangluong-ah.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/cdm2024/',
