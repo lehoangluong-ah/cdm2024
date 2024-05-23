b883ecbd5c666c3ca6660104ddd86532c58bc1e4
ngocNC <ngoc-nc.nguyen@amplifyhealth.com>
2024-05-23 16:37:46 +0700

    change url link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
   url: 'https://lehoangluong-ah.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cdm2024/',
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
